7a4975c494b4509fcf34d6e0537f2ae62c51c5b0
Seva-Sh <skimin1992@hotmail.com>
2024-11-07 15:57:46 +0400

    Created Static Components

---
+++ src/Tracklist.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+
+// request data from spotify, json response contains a set of song tracks
+// jamming app should display the song name artist and album for each track in the results list
+// create a unidirectional data flow from the root component
+// the root component should pass down the search results to a child component that will
+// return each individual track to be rendered
+
+export default App;
